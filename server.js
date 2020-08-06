af73d32dddf4404712801bea175a6de595f276f2
Hernan Case <her.case@live.com.ar>
2020-08-06 14:28:56 -0300

    Added SSL

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const db = require("./models");
 const express = require('express');
 const app = express();
+const https = require("https");
+const fs = require("fs");
 
 const corsOptions = {
   origin: "*",
@@ -13,6 +15,12 @@ app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/batan.tv/fullchain.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/batan.tv/privkey.pem")
+};
+
 
 db.mongoose
   .connect(db.url, {
@@ -32,6 +40,6 @@ db.mongoose
 // set port, listen for requests
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
