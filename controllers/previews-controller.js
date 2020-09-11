49aa8b7956f78cc326fd041e1c99eb2018cc149a
Hernan Case <her.case@live.com.ar>
2020-09-11 13:40:23 -0300

    added: getChannelsIDs method.

--- controllers/previews-controller.js
+++ controllers/previews-controller.js
@@ -1,6 +1,6 @@
-const { response } = require('express');
-
 const axios = require('axios').default;
+const db = require("../models");
+const Channel = db.channels;
 
 const formatResponses = (object) => {
   let newFormat = [];
@@ -23,8 +23,29 @@ const formatResponses = (object) => {
   return newFormat;
 };
 
+const getChannelsIDs = ( random , res) => {
 
-exports.updatePreview = (channel_id, res) => {
+  Channel.find({})
+  .then(data => {
+    let transformedResponse = JSON.stringify(data);    
+    let channels_ids = JSON.parse(transformedResponse).map(item => item.media.youtube_id);
+    res.send(channels_ids) 
+
+  })
+  .catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while retrieving channels."
+    });
+  });
+};
+
+const updatePreview = () => {
+  
+}
+
+
+exports.updatePreview2 = (channel_id, res) => {
 
   const key = 'AIzaSyAxgOxgraVNuvTyYdz9iy3E6DDLbOwEZwo';
 
